101bed976a0ec753dacab7b0ea20d08245bbf2fe
hashimoto <hashimoto227@yamakawa-p.jp>
2024-09-28 15:05:21 +0900

    3章まで完了

---
+++ app/_components/Footer/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./index.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <nav className={styles.nav}>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <a href="/news">ニュース</a>
+          </li>
+          <li className={styles.item}>
+            <a href="/members">メンバー</a>
+          </li>
+          <li className={styles.item}>
+            <a href="/contact">お問い合わせ</a>
+          </li>
+        </ul>
+      </nav>
+      <p className={styles.cr}>🄫　SiMPLE. ALL Rights Reserved 2024</p>
+    </footer>
+  );
+}
