2b997ac8937fc9fc89ec3ff32259f3dfc203f244
hashimoto <hashimoto227@yamakawa-p.jp>
2024-10-09 13:40:26 +0900

    5章まで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,48 +1,10 @@
 import Image from "next/image";
+import { getMembersList } from "@/app/_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
 import styles from "./page.module.css";
 
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッド・チャン",
-      position: "CEO",
-      profile:
-        "グローバルテクノロジー企業で豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部してらりるれろ",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member2.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "エミリー・キラー",
-      position: "COO",
-      profile:
-        "グローバルテクノロジー企業で豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部してらりるれろ",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member3.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "ウィルキン・ソン",
-      position: "CTO",
-      profile:
-        "グローバルテクノロジー企業で豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部してらりるれろ",
-    },
-  ],
-};
-
-export default function Page() {
+export default async function Page() {
+  const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
